65a21f0fa6bc75911d5ace8ae268edee7e41d760
JonnyLoo <jon.liu1997@gmail.com>
2020-02-04 20:14:43 -0500

    change put to patch

--- routes/v1/quotes.js
+++ routes/v1/quotes.js
@@ -6,6 +6,6 @@ const controller = require('../../controllers');
 ROUTER.get('/:id', controller.getQuote);
 ROUTER.post('/create', controller.createQuote);
 ROUTER.delete('/:id', controller.deleteQuote);
-ROUTER.put('/:id', controller.updateQuote);
+ROUTER.patch('/:id', controller.updateQuote);
 
 module.exports = ROUTER;
