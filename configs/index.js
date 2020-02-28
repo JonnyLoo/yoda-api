ced149a5530e2cd17ab083ea82c7f40e026ed833
Jon Liu <Jon.liu1997@gmail.com>
2020-02-27 22:25:48 -0500

    change mongo url

--- configs/index.js
+++ configs/index.js
@@ -1,6 +1,6 @@
 const PORT = 3001;
 
-const DB_URL = 'mongodb+srv://baby-yoda:admin@yoda-atohj.mongodb.net/test?retryWrites=true&w=majority';
+const DB_URL = 'mongodb+srv://yoda-ui:admin@yoda-atohj.mongodb.net/test?retryWrites=true&w=majority';
 const DB_URL_LOCAL = 'mongodb://localhost/yoda';
 
 module.exports = {
